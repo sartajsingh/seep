04616385cbcf830d4c84c36e82cd263846727891
Sartaj Singh <srtaj.sngh@gmail.com>
2015-10-08 20:34:18 +0530

    [sartaj] cards divided into arrays between players and initial floor cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,22 +8,33 @@ class CardsController < ApplicationController
 
   def two_players_game shuffled_cards
     @player_one_must_card = shuffled_cards.where(value: %w(9 10 J Q K)).first
-    @player_one_rest_cards = shuffled_cards.first(limit = "3")
-    #shuffled_cards = shuffled_cards.where.not("value = ? and suit = ?", [@player_one_must_card.value, @player_one_rest_cards[0].value, @player_one_rest_cards[1].value, @player_one_rest_cards[2].value], [@player_one_must_card.suit, @player_one_rest_cards[0].suit, @player_one_rest_cards[1].suit, @player_one_rest_cards[2].suit])
+    shuffled_cards = shuffled_cards.where.not("value = ? and suit = ?", @player_one_must_card.value, @player_one_must_card.suit)
+
+    @initial_floor_cards = shuffled_cards.first(limit = "4")
+    shuffled_cards = shuffled_cards.where.not("value = ? and suit = ?", @initial_floor_cards[0].value, @initial_floor_cards[0].suit).where.not("value = ? and suit = ?", @initial_floor_cards[1].value, @initial_floor_cards[1].suit).where.not("value = ? and suit = ?", @initial_floor_cards[2].value, @initial_floor_cards[2].suit).where.not("value = ? and suit = ?", @initial_floor_cards[3].value, @initial_floor_cards[3].suit)
 
     shuffled_cards = shuffled_cards.in_groups(2)
-    @player_one_cards = shuffled_cards[0]
-    @player_two_cards = shuffled_cards[1]
+    @player_two_cards = shuffled_cards[0]
+    shuffled_cards[1].pop
+    @player_one_cards = shuffled_cards[1] << @player_one_must_card
+    @initial_floor_cards = @player_one_rest_cards
     return @initial_floor_cards, @player_one_cards, @player_two_cards
   end
 
   def four_players_game shuffled_cards
-    shuffled_cards = shuffled_cards.each_slice((52 - 4) / 4).to_a
-    @player_one_cards = shuffled_cards[0]
-    @player_two_cards = shuffled_cards[1]
-    @player_three_cards = shuffled_cards[2]
-    @player_four_cards = shuffled_cards[3]
-    @initial_floor_cards = shuffled_cards[4]
+    @player_one_must_card = shuffled_cards.where(value: %w(9 10 J Q K)).first
+    shuffled_cards = shuffled_cards.where.not("value = ? and suit = ?", @player_one_must_card.value, @player_one_must_card.suit)
+
+    @initial_floor_cards = shuffled_cards.first(limit = "4")
+    shuffled_cards = shuffled_cards.where.not("value = ? and suit = ?", @player_one_rest_cards[0].value, @player_one_rest_cards[0].suit).where.not("value = ? and suit = ?", @player_one_rest_cards[1].value, @player_one_rest_cards[1].suit).where.not("value = ? and suit = ?", @player_one_rest_cards[2].value, @player_one_rest_cards[2].suit).where.not("value = ? and suit = ?", @player_one_rest_cards[3].value, @player_one_rest_cards[3].suit)
+
+
+    shuffled_cards = shuffled_cards.in_groups(4)
+    @player_two_cards = shuffled_cards[0]
+    @player_three_cards = shuffled_cards[1]
+    @player_four_cards = shuffled_cards[2]
+    shuffled_cards[3].pop
+    @player_one_cards = shuffled_cards[3] << @player_one_must_card
     return @initial_floor_cards, @player_one_cards, @player_two_cards, @player_three_cards, @player_four_cards
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007131921) do
+ActiveRecord::Schema.define(version: 20151007152126) do
 
   create_table "cards", force: :cascade do |t|
     t.string   "suit"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20151007131921) do
   end
 
   create_table "decks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "games", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
